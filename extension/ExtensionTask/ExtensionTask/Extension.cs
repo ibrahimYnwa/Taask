a77dea59ef1e419a68961229e1136eab2dc0f3d7
ibrahim <ibrahimea@code.edu.az>
2021-10-13 23:22:00 +0400

    Extension

---
+++ extension/ExtensionTask/ExtensionTask/Extension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExtensionTask
+{
+    static class Extension
+    {
+        #region
+        public static bool Palindrom(this int number)
+        {
+            int n = 0;
+            int result = number;
+
+            while (result > 0)
+            {
+                n = n * 10 + result % 10;
+                result /= 10;
+            }
+            return n == number;
+
+
+        }
+        #endregion Palindrom Palindrom
+
+
+
+
+
+    }
+}
+
+
+
---
+++ extension/ExtensionTask/ExtensionTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int a = 121;
+            Console.WriteLine(a.Palindrom());
+            #endregion
+
+
+        }
+
+    }
+}
